816ba2bf89a6af889513a46432b7617fc7ae8cdc
Jonathan Paré <jpare@dexiom.com>
2017-04-16 21:17:29 -0400

    Add nuspec

--- Dexiom.Serialization/Dexiom.Serialization/Properties/AssemblyInfo.cs
+++ Dexiom.Serialization/Dexiom.Serialization/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Dexiom.Serialization")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Tiny library designed to help with object serialization.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Dexiom")]
 [assembly: AssemblyProduct("Dexiom.Serialization")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("© Consultation Dexiom inc.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
